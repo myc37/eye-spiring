77aa2133a04a843acc2b60be15a318e840561d85
myc37 <nvjn37@gmail.com>
2022-02-12 13:16:31 +0800

    update boilerplate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import About from "./pages/About";
+import Test from "./pages/Test";
 import Home from "./pages/Home";
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
 		<div>
 			<Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="about" element={<About />} />
+				<Route path="test" element={<Test />} />
 			</Routes>
 		</div>
 	);
--- src/components/Footbar.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Footbar = () => {
-	return <div>Footbar</div>;
-};
-
-export default Footbar;
